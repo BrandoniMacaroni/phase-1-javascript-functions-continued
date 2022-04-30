a20bdce78e095b4e76ebed86fbeff468b9725ca0
Brandon <bcharris713@gmail.com>
2022-04-29 19:26:31 -0500

    commit tree be9d037cfca61e455e1178db23c77c9ee8c3e732

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (typeof activity === "undefined") {
+        activity = "This Saturday, I want to roller-skate!";
+      }
+    else {
+        activity = 'This Saturday, I want to ' + activity + '!';
+    }
+    return activity;
+}
+
+function mondayWork(activity) {
+    if (typeof activity === "undefined") {
+        activity = "This Monday, I will go to the office.";
+    }
+    else {
+        activity = 'This Monday, I will ' + activity + '.';
+    }
+    return activity;
+}
+
+function wrapAdjective(saying) {
+    const innerFunction = function (visualFlair = "special.") {
+      return `You are ${saying}${visualFlair}${saying}!`;
+    };
+    return innerFunction;
+}
+
